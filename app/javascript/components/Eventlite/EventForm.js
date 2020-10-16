8dfe3f7d3b3b78b262f4ed12feb7162a85cc9569
Agustino Saling <agustino@saling.eu>
2020-10-16 23:02:01 +0200

    create a new event

---
+++ app/javascript/components/Eventlite/EventForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+import axios from "axios";
+
+class EventForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      start_datetime: "",
+      location: "",
+    };
+  }
+
+  handleInput = (e) => {
+    const name = e.target.name;
+    const newState = {};
+    newState[name] = e.target.value;
+    this.setState(newState);
+    e.preventDefault();
+  };
+
+  handleSubmit = (e) => {
+    axios({
+      method: "POST",
+      url: "/appointments",
+      data: { event: this.state },
+      headers: {
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']")
+          .content,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        this.props.handleNewEvent(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Create an Event:</h4>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={this.handleInput}
+          />
+          <input
+            type="text"
+            name="start_datetime"
+            placeholder="Date"
+            value={this.state.start_datetime}
+            onChange={this.handleInput}
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="Location"
+            value={this.state.location}
+            onChange={this.handleInput}
+          />
+          <button type="submit">Create Event</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EventForm;
--- app/javascript/packs/Eventlite.js
+++ app/javascript/packs/Eventlite.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import EventList from "../components/Eventlite/EventList";
+import EventForm from "../components/Eventlite/EventForm";
 
-const Eventlite = (props) => <EventList events={props.events} />;
+class Eventlite extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: this.props.events,
+    };
+  }
+
+  addNewEvent = (event) => {
+    const events = [event, ...this.state.events].sort(function (a, b) {
+      return new Date(a.start_datetime) - new Date(b.start_datetime);
+    });
+    this.setState({ events: events });
+  };
+
+  render() {
+    return (
+      <>
+        <EventList events={this.state.events} />;
+        <EventForm handleNewEvent={this.addNewEvent} />
+      </>
+    );
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const node = document.getElementById("events_data");
