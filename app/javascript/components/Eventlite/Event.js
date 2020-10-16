0fe35be4b32fe7aa5ebe0950d355c9c5cbd02685
Agustino Saling <agustino@saling.eu>
2020-10-16 21:47:59 +0200

    event with static data

--- app/javascript/components/Eventlite/Event.js
+++ app/javascript/components/Eventlite/Event.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Event = (props) => <li>{props.event.title}</li>;
+const Event = (props) => (
+  <li>
+    <b>{props.event.title}</b>
+    <span>{props.event.datetime}</span>
+    <span>{props.event.location}</span>
+  </li>
+);
 
 export default Event;
